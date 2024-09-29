f15b653a37ba2fabb8566f639bde8550dd0e607a
Ritii-13 <ritika22408@iiitd.ac.in>
2024-09-29 20:29:09 +0530

    add changes

--- Ass4/TCPServer.py
+++ Ass4/TCPServer.py
@@ -7,6 +7,7 @@ serverSocket = socket(AF_INET, SOCK_STREAM)
 serverPort = 6789  
 serverSocket.bind(('0.0.0.0', serverPort))
 serverSocket.listen(1)
+print('The server is ready to receive')
 
 
 while True:
@@ -16,33 +17,40 @@ while True:
 
     try:
         message = connectionSocket.recv(1024).decode()
-
         if not message:
             connectionSocket.close()
             continue
 
         filename = message.split()[1]
         f = open(filename[1:])
+
+        print("Requested file: " + filename)
+
         outputdata = f.read()
 
         # for part 1
-        # connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
-        # for i in range(0, len(outputdata)):
-        #     connectionSocket.send(outputdata[i].encode())
+        connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
+        for i in range(0, len(outputdata)):
+            connectionSocket.send(outputdata[i].encode())
         
-        # For client in part 3 next 2 lines and comment out above code
-        response = "HTTP/1.1 200 OK\r\n\r\n" + outputdata
-        connectionSocket.send(response.encode())
+        # # For client in part 3 next 2 lines and comment out above code
+        # response = "HTTP/1.1 200 OK\r\n\r\n" + outputdata
+        # connectionSocket.send(response.encode())
 
-        print(response)
+        print("Sent response to client")
+
+        # print(response)
 
         connectionSocket.send("\r\n".encode())
         f.close()
         connectionSocket.close()
+
+        print("Connection closed.")
         
     except IOError:
         connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
         connectionSocket.send("<html><body><h1>404 Not Found</h1></body></html>\r\n".encode())
+        print("File not found, sending 404 response.")
         connectionSocket.close()
 
 serverSocket.close()
